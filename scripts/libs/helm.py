a6655476ee7deb54684f297033bf95869abbf928
Frank Wickström <frank.wickstrom@anders.fi>
2020-02-20 17:35:39 +0200

    Up the Helm revision history from 10 to 30
    As we noticed that there are times that 10 failed releases
    can be deployed after eachother and after that there is no
    way to get back to a proper release again with the `helm rollback`
    command. This will at least make it less plausable for that
    to happen.

--- scripts/libs/helm.py
+++ scripts/libs/helm.py
@@ -117,6 +117,8 @@ class Helm:
             "--wait",
             "--timeout",
             "180s",
+            "--history-max",
+            "30",
             install_arg,
             "--namespace",
             f"{namespace}",
