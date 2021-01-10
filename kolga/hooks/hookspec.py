c3ac53b4ccb40aa2d5c41ded926bf5ee77bbadc5
Frank Wickström <frank.wickstrom@anders.fi>
2021-01-10 15:26:44 +0200

    Add project_deployment_complete hook

--- kolga/hooks/hookspec.py
+++ kolga/hooks/hookspec.py
@@ -1,12 +1,34 @@
-from typing import Any, Callable, TypeVar, cast
+from typing import TYPE_CHECKING, Any, Callable, Optional, TypeVar, cast
 
 import pluggy  # type: ignore
 
 F = TypeVar("F", bound=Callable[..., Any])
 hookspec = cast(Callable[[F], F], pluggy.HookspecMarker("kolga"))
 
+if TYPE_CHECKING:
+    from kolga.libs.project import Project
+
 
 class KolgaHookSpec:
     """
     Kolga Hook Specification
     """
+
+    @hookspec
+    def project_deployment_complete(
+        self, project: "Project", track: str, namespace: str
+    ) -> Optional[bool]:
+        """
+        Fired when a new deployment of a project has completed successfully.
+
+        Args:
+            namespace: Namespace of the deployment
+            track: Track of the deployment
+            project: A `Project` object including all information about the project
+
+        Returns:
+            Optionally returns a boolean value denoting if the plugin
+            finished successfully.
+
+            The return value is not acted upon by Kólga.
+        """
---
+++ tests/test_hooks.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+from environs import Env
+
+from kolga.hooks import hookimpl
+from kolga.hooks.plugins import PluginBase
+from kolga.libs.project import Project
+from kolga.settings import settings
+from tests.testcase import load_plugin
+
+
+class _TestPlugin(PluginBase):
+    name = "test_plugin"
+    verbose_name = "Kolga Example Plugin"
+    version = 0.1
+
+    def __init__(self, env: Env) -> None:
+        self.configure(env)
+
+    @hookimpl
+    def project_deployment_complete(self, project: Project) -> Optional[bool]:
+        if not self.configured:
+            return None
+
+        return True
+
+
+@load_plugin(_TestPlugin)
+def test_create_deployment() -> None:
+    results = settings.plugin_manager.hook.project_deployment_complete(
+        project=Project(track="testing", url="test.example.com"),
+        track="testing",
+        namespace="testing",
+    )
+
+    assert len(results) and results[0] is True
