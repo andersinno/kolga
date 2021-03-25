9f99fb8860c8a9ec1a87df8552d7c735ff29da06
Antti Tanhuanpää <antti.tanhuanpaa@anders.fi>
2021-03-25 11:06:57 +0200

    tests: Allow for running tests without Docker

--- utils/test_utils.sh
+++ utils/test_utils.sh
@@ -1,15 +1,27 @@
-#!/usr/bin/env bash
+check_docker() {
+    echo
+    echo "###########################"
+    echo "### Checking for Docker ###"
+    echo "###########################"
 
+    if [ -z "$CONTAINER_REGISTRY" ]; then
+	return 1
+    fi
 
-function setup_kubernetes() {
-    echo -e "\n#############################"
+    local HOST=${CONTAINER_REGISTRY//:*}
+    local PORT=${CONTAINER_REGISTRY##*:}
+
+    wait_for_tcp "$HOST" "$PORT"
+    export TEST_DOCKER_ACTIVE=1
+}
+
+setup_kubernetes() {
+    echo
+    echo "#############################"
     echo "### Setting up Kubernetes ###"
     echo "#############################"
-    until nc -z -v -w30 kubernetes 6443
-    do
-        echo "Waiting for Kubernetes cluster to respond"
-        sleep 1
-    done
+
+    wait_for_tcp kubernetes 6443
 
     # Give some time for the service to create the required config files
     sleep 10
@@ -26,4 +38,20 @@ function setup_kubernetes() {
 
     echo "Settings up local storage for nodes"
     kubectl apply -f tests/manifests/local-path-storage.yaml
+}
+
+wait_for_tcp() {
+    HOST=$1
+    PORT=$2
+    N_RETRIES=${3-90}
+
+    echo "Waiting for $HOST:$PORT..."
+    until [ $N_RETRIES -lt 1 ]; do
+	nc -z -w15 "$HOST" "$PORT" 2>/dev/null && return
+	sleep 1
+	(( N_RETRIES-- ))
+    done
+
+    echo "FAILED to connect $HOST:$PORT!"
+    false
 }
