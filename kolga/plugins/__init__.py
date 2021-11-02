823ac445afd8f8ff025e6d127fed0b16e603c1bf
Antti Tanhuanpää <antti.tanhuanpaa@anders.fi>
2021-11-02 11:46:21 +0200

    kolga.plugins: Implement automatic discovery

--- kolga/plugins/__init__.py
+++ kolga/plugins/__init__.py
@@ -1,10 +1,25 @@
-from typing import Tuple, Type
+from importlib import import_module
+from pathlib import Path
+from typing import Generator, Optional, Tuple, Type
 
 from kolga.plugins.base import PluginBase
-from kolga.plugins.sentry.sentry import KolgaSentryPlugin
-from kolga.plugins.slack.slack import KolgaSlackPlugin
+from kolga.utils.logger import logger
 
-KOLGA_CORE_PLUGINS: Tuple[Type[PluginBase], ...] = (
-    KolgaSentryPlugin,
-    KolgaSlackPlugin,
-)
+
+def _import_plugins(
+    directory: Optional[Path] = None,
+) -> Generator[Type[PluginBase], None, None]:
+    if not directory:
+        directory = Path(__file__).parent
+
+    for init_file in directory.glob("*/__init__.py"):
+        module_name = init_file.parent.name
+        module = import_module(f".{module_name}", package=__package__)
+
+        try:
+            yield getattr(module, "Plugin")
+        except AttributeError:
+            logger.warning(f"Unable to load plugin: {module_name}")
+
+
+KOLGA_CORE_PLUGINS: Tuple[Type[PluginBase], ...] = tuple(_import_plugins())
